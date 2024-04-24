0e14ea1e205928516d833a6649b166fc366afdf2
zlooper23 <157705546+zlooper23@users.noreply.github.com>
2024-04-24 11:06:04 +0200

    Add files via upload

---
+++ rpg_base.h
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#define MAX_NAME 30 
+#define MAX_TXT 200 
+
+typedef struct{
+    char name[MAX_NAME];
+    char description[MAX_TXT];
+    int of_def; //1 for offensive, 0 for deffensive
+    int dmg_skll;
+    int modifier;
+    int dmg_plyr; //3 - 8
+    int def_plyr; //0 - 8
+    int hp_plyr; //25 - 50
+}Skill;
+
+
+Skill:
+dmg:uigfhfguh,
+fscanf("dg:%s,")
