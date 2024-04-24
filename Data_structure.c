f24bd1fa0e12c7eb0c6e65981cbfc33d9c11e7bd
zlooper23 <157705546+zlooper23@users.noreply.github.com>
2024-04-24 10:41:46 +0200

    Update Data_structure.c

--- Data_structure.c
+++ Data_structure.c
@@ -1,4 +1,11 @@
 typedef struct 
 {
-    
+    char name[MAX_NAME];
+    char description[MAX_TXT];
+    int of_def; //1 for offensive, 0 for deffensive
+    int dmg_skll;
+    int modifier;
+    int dmg_plyr; //3 - 8
+    int def_plyr; //0 - 8
+    int hp_plyr; //25 - 50    
 }Skills;
