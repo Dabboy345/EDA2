94c38ebb045bf70e4a1c46277545463bcac74008
Sushant <sushant.pokhrel01@estudiant.upf.edu>
2024-05-08 09:34:50 +0200

    Example of how we would like to implement our code  (Not funcional)

--- rpg_base.c
+++ rpg_base.c
@@ -1,12 +1,28 @@
-#include"rpg_base.h"
+#include "rpg_base.h"
+#include "rpg_game.c"
+#include "common.h"
 
 void new_game(){//Our new game function
     Skill *skill = (Skill*)calloc(20, sizeof(Skill));
     get_skill(skill);
     create_character(skill);
-
     //Decision* main_decision = decision[0];
 
+    /*This is an example of the implatiaion of what we would like to do, we will create/add functions necessary to make it more automatic
+    Node *root = create_node();
+    add_Node_left(root);
+    add_Node_right(root);
+    FILE *c = fopen("desicion.txt","r");
+    fgets(root->descions.question_txt, MAX_TXT, c);//This will get your descriptions of where we are
+    fgets(root->descions.option[0], MAX_TXT, c);//Our first options 
+    fgets(root->descions.option[1], MAX_TXT, c);//Our second options 
+    //Now here we will print the options and make the player chose it after the selection we will go to the diffreent node 
+    //and repeat the same process to play the game
+
+    //Things needed to do this part function to print the despcription,options.. part of each node and function to set the selected
+    //node by the user and take the necessary infotmation from the corresponding txt file.*/ 
+
+    //Imporatant things to take into account error handaling, free the memory created dnamically for each node(create this function)
 }
 
 void save_game(){//Our save game function
@@ -22,9 +38,9 @@ void exit_game(){//Our exit game function
 }
 
 int main(){
-    print_menu();
+    print_menu();//We will print the menu
     int a;
-    fscanf("%d", &a);
+    fscanf("%d", &a);//We will ask the user what option would he like to choose
     switch(a){
         case 1:
             new_game();
